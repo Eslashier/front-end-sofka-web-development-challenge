b4beda88b1358188871c5e28e804895348dc1ba1
Eslashier <anfelov@gmail.com>
2022-05-05 20:42:24 -0500

    added cases to reducer function

--- src/App.jsx
+++ src/App.jsx
@@ -11,26 +11,5 @@ function App() {
   )
 }
 
-useEffect(()=>{
-  let listOfNote = fetchAllNotes().then(
-      notes=>{
-        console.log(data)
-          // let action = {
-          //     type:'get-notes',
-          //     payload: notes
-          // }
-
-          // dispatch(action)
-      }
-  )
-
-},[])
-
-const fetchAllNotes = async()=>{
-  let response = await fetch(`http://localhost:8081/api/get/notes`);
-  let data = await response.json();
-  console.log(data)
-  return data
-}
 
 export default App
---
+++ src/components/Reducer.jsx
@@ -0,0 +1,18 @@
+function reducer(state, action){
+    switch(action.type){
+        case 'get-lists':
+            return state
+        case 'add-list':
+            return state
+        case 'add-task':
+            return state
+        case 'remove-list':
+            return state
+        case 'remove-task':
+            return state
+        case 'update-task':
+            return state
+    }
+}
+
+export default reducer
