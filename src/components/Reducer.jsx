11860c15800de4f5a782e9579313122240d9812e
Eslashier <anfelov@gmail.com>
2022-05-06 15:49:22 -0500

    add tasks in any list

--- src/components/Reducer.jsx
+++ src/components/Reducer.jsx
@@ -1,52 +1,53 @@
-function reducer(state, action){
-    switch(action.type){
+function reducer(state, action) {
+    switch (action.type) {
         case 'get-lists':
             return state
+
         case 'add-group':
             const newGroup = {
-                id: Math.floor(Math.random()*100),
+                id: Math.floor(Math.random() * 100),
                 name: action.payload.group,
-                todo:[]
+                todo: []
             }
             const newListOfGroupsAdded = [...state.listOfTitles, newGroup]
-            const newStateAddGroup={
+            const newStateAddGroup = {
                 ...state, listOfTitles: newListOfGroupsAdded
             }
             console.log(newStateAddGroup)
             return newStateAddGroup;
+
         case 'add-task':
             const newTask = {
-                id: Math.floor(Math.random()*100),
+                id: Math.floor(Math.random() * 100),
                 taskToDo: action.payload.task,
                 done: false,
                 fkTitleId: action.payload.fk
             }
+
+            const index = state.listOfTitles.findIndex(element => element.id === action.payload.fk)
             
-            const groupWithoutTask = state.listOfTitles.find(element => element.id === action.payload.fk)
-
-            const index = groupWithoutTask.id
-
             const newListOfTasks = [...state.listOfTitles[index].todo, newTask]
 
-            const updatedGroup={
+            const updatedGroup = {
                 ...state.listOfTitles[index], todo: newListOfTasks
             }
 
-            const listToUpdate=[...state.listOfTitles]
+            const listToUpdate = [...state.listOfTitles]
 
             listToUpdate[index] = updatedGroup
 
-            const updatedMainList={
-                ...state, listOfTitles : listToUpdate
+            const updatedMainList = {
+                ...state, listOfTitles: listToUpdate
             }
 
-            console.log(updatedMainList)
-            return(updatedMainList)
+            return (updatedMainList)
 
         case 'remove-list':
             return state
+
         case 'remove-task':
             return state
+
         case 'update-task':
             return state
     }
